537d439b848e12a4a450c0e16eb91f595baf749c
wbasp <maad7445@gmail.com>
2022-12-31 19:01:46 +0600

    position find

--- phone.js
+++ phone.js
@@ -11,18 +11,18 @@ const displayResult = phones =>{
     const searchResult = document.getElementById('search-result');
     searchResult.innerHTML=``;
     phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML=`<div>
-        <div class="card h-100">
-            <img src="${phone.image}" class="card-img-top w-50 h-50 mx-auto" alt="${phone.slug}">
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('col');
+        newDiv.innerHTML=`<div>
+        <div class="card h-100 w-75 mx-auto">
+            <img src="${phone.image}" class="card-img-top w-50 h-50 mx-auto mt-4 mb-0" alt="${phone.slug}">
             <div class="card-body">
                 <h5 class="card-title text-center">${phone.phone_name}</h5>
                 <p class="card-text text-center">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
-            <button class="bg-primary mb-2 p-2 rounded-pill w-50 mx-auto">Phone Details</button>
+            <button class="bg-warning fw-bold text-danger mb-3 p-2 rounded-pill w-50 mx-auto">Phone Details</button>
         </div>
         </div>`;
-        searchResult.appendChild(div);
+        searchResult.appendChild(newDiv);
     });
 }
